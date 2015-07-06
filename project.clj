1ccedb2a12eabc4f58e12af07eb08e3dddc1390e
6s6 <info@espaciofontana.org>
2015-07-06 12:50:27 +0200

    port

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-ring "0.9.2"]]
-  :ring {:handler liberator-example.core/handler}
+  :ring {:handler liberator-example.core/handler
+         :port 3000}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  ;; totp support
                  [cylon "0.5.1" :exclusions [org.clojure/clojure]]
